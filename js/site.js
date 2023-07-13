a4ece69d73f1a142f85a7660f8641bdece4a1895
[murilodab] <[muriloab96@gmail.com]>
2023-07-13 10:22:29 -0300

    Added JS code for the app page, and updated code.html

--- js/site.js
+++ js/site.js
@@ -1,4 +1,69 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World");
+
+//get the values from the page
+//starts or controller function
+function getValues() {
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    //We need to validate our input
+    //parse into Integers
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+
+        //we call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+
+        //we call displayNumbers
+        displayNumbers(numbers);
+
+    } else {
+        alert("You must enter Integers");
+    }
+
+
+
+}
+
+//generate numbers from starValue to endValue
+//logic function(s)
+function generateNumbers(sValue, eValue) {
+
+    let numbers = [];
+    //we want to get all numbers from start to end
+    for (let index = sValue; index <= eValue; index++) {
+
+        //This will execute in a loop until index = eValue
+
+        numbers.push(index);
+
+    }
+    return numbers;
+
+}
+
+//display the numbers and mark odd numbers bold
+//display or view functions
+function displayNumbers(numbers) {
+
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+
+        let className = "odd";
+
+        let number = numbers[index];
+        if (number % 2 !== 0) {
+            className = "odd";
+        } else {
+            className = "even";
+        }
+
+        templateRows += `<tr><td class="${className}">${number}</td=></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
+
 }
